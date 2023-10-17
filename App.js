df4808a1d06056cec97b52547e46f89180c4ab17
MARCOS <marcossuarezr88@gmail.com>
2023-10-17 18:48:11 +0200

    configuring autenthificator gooD

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
+import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import Navigator from "./src/navigation";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Amplify } from "aws-amplify";
+import { Amplify, Auth, DataStore } from "aws-amplify";
 import awsconfig from "./src/aws-exports";
+import { withAuthenticator } from "aws-amplify-react-native";
+// import UserContextProvider from "./src/contexts/UserContext";
 
-Amplify.configure(awsconfig);
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
 
 
-export default function App() {
+function App() {
+
+  const [authUser, setAuthUser] = useState(null);
+
+  useEffect(() => {
+    Auth.currentAuthenticatedUser().then(setAuthUser);
+  }, []);
+
+  console.log(authUser);
+
   return (
     <SafeAreaProvider style={styles.container}>
       <Navigator />
@@ -25,3 +37,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#c9c9c9",
   },
 });
+
+export default withAuthenticator(App);
